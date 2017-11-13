9cfe2ef5220b68f32c7e5b2904624129bd434440
Matthew Volz <matthew.c.volz@gmail.com>
2017-11-13 16:48:21 -0600

    adding scoreboard command

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var http, director, router, server, port, redis, pug, express, app;
+var http, director, router, server, port, redis, pug, express, app, https;
 
-http        = require('http');
+http = require('http');
+https = require('https');
 director    = require('director');
 redis		= require('redis').createClient(process.env.REDIS_URL);
-pug			= require('pug');
+pug = require('pug');
+
+var botID = process.env.BOT_ID;
 
 
 router = new director.http.Router({
@@ -260,19 +263,60 @@ function onMessageReceived()
 {
 	var data, timeRegex, timeString, cur;
 	data = JSON.parse(this.req.chunks[0]);
-	timeRegex = /\[.*?\]/m
+    timeRegex = /\[.*?\]/m
 
-	timeString = timeRegex.exec(data.text);
+    if (data.text === "/scoreboard")
+    {
+        postMessage("https://crossword-leaderboard.herokuapp.com");
+    }
+    else
+    {
+        timeString = timeRegex.exec(data.text);
 
-	if (timeString)
-	{
-		timeString = timeString[0].replace("[", "");
-		timeString = timeString.replace("]", "");
-		addNewTime(data, timeString);
-	}
+        if (timeString) {
+            timeString = timeString[0].replace("[", "");
+            timeString = timeString.replace("]", "");
+            addNewTime(data, timeString);
+        }
 
-	this.res.writeHead(200);
-	this.res.end();
+        this.res.writeHead(200);
+        this.res.end();
+    }
+
+	
+}
+
+function postMessage(data) {
+    var botResponse, options, body, botReq;
+
+    botResponse = data;
+
+    options = {
+        hostname: 'api.groupme.com',
+        path: '/v3/bots/post',
+        method: 'POST'
+    };
+
+    body = {
+        "bot_id": botID,
+        "text": botResponse
+    };
+
+    botReq = HTTPS.request(options, function (res) {
+        if (res.statusCode == 202) {
+            //neat
+        } else {
+            console.log('rejecting bad status code ' + res.statusCode);
+        }
+    });
+
+    botReq.on('error', function (err) {
+        console.log('error posting message ' + JSON.stringify(err));
+    });
+    botReq.on('timeout', function (err) {
+        console.log('timeout posting message ' + JSON.stringify(err));
+    });
+    botReq.end(JSON.stringify(body));
 }
 
 function addNewTime(data, timeString)
